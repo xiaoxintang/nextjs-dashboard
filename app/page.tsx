5590d71d7f4a5332e83672ccd17b558b0bcc53f2
longan <15707971810@163.com>
2024-03-10 23:32:50 +0800

    fix:mobile png

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image src='/hero-desktop.png' width={1000} height={760} className='hidden md:block' alt='截图' />
-          <Image src='/hero.png' width={560} height={620} className='block md:hidden' alt='截图' />
+          <Image src='/hero-mobile.png' width={560} height={620} className='block md:hidden' alt='截图' />
 
         </div>
 
